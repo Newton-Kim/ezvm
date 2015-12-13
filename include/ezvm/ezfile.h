53fe3a721dba72740d1f87de252b1b10c0dc274e
Newton Kim <Newton-Kim@github.com>
2015-12-13 13:49:39 +0900

    ezFile class has been documented.

--- include/ezvm/ezfile.h
+++ include/ezvm/ezfile.h
@@ -29,17 +29,64 @@
 
 using namespace std;
 
+/**
+* @brief ezFile is a wrapper of the file system for ease of use in ezVM.
+*/
 class ezFile {
  private:
+/**
+* @brief is a file descriptor.
+*/
   FILE* m_fd;
 
  public:
+/**
+* @brief is a default constructor. It creates a null file object, which does nothing like /dev/null.
+*/
   ezFile();
+/**
+* @brief creates a file object whose name is path. The argument mode follows the rule of the mode of fopen.
+*
+* @param path is a file name with directory names delimited by /.
+* @param mode is collection of mode descriptors described in the mode of fopen.
+*/
   ezFile(const string path, const string mode);
+/**
+* @brief is a default destructor. It closes the file which is opened in the constructors.
+*/
   ~ezFile();
+/**
+* @brief produces output according to a format as described fprintf.
+*
+* @param fmt is a format string, which is collection of directives. See that of fprintf in 'man fprintf'.
+* @param ... is variable arguments.
+*/
   void print(const char* fmt, ...);
+/**
+* @brief produces output according to a format 
+*
+* @param fmt is a format string, which is collection of directives. See that of fprintf in 'man fprintf'.
+* @param ap is va_list argument.
+*/
   void vprint(const char* fmt, va_list ap);
+/**
+* @brief is an equivalent funciton to print, but it is followed by a line separation such as '\n'.
+*
+* @param fmt is a format string, which is collection of directives. See that of fprintf in 'man fprintf'.
+* @param ... is variable arguments.
+*/
   void println(const char* fmt, ...);
+/**
+* @brief is an equivalent funciton to vprint, but it is followed by a line separation such as '\n'.
+*
+* @param fmt is a format string, which is collection of directives. See that of fprintf in 'man fprintf'.
+* @param ap is va_list argument.
+*/
   void vprintln(const char* fmt, va_list ap);
+/**
+* @brief shows if the file object is nill object.
+*
+* @return is true if the file object is equivalent to /dev/null, and false, otherwise.
+*/
   bool isnil(void) { return (m_fd) ? false : true; }
 };
